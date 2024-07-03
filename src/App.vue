abb218f309f94d06a7b263018620ec56a8f41021
RSamaium <sam@newbot.io>
2024-07-03 11:36:22 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    {{ count }}
+    <Navbar />
+</template>
+
+<script lang="ts">
+import Navbar from './components/navbar/Navbar.vue';
+
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    components: {
+        Navbar
+    }
+}
+</script>
