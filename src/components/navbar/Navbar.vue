26b17cdd809825d46e1107a263751ac6ec6c5fc6
RSamaium <sam@newbot.io>
2024-07-03 12:28:39 +0200

    search

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,9 +1,25 @@
 <template>
-    <h1>Mon App</h1>
+    <h1>{{ title }}</h1>
+    <Search :name="username" />
 </template>
 
 <script lang="ts">
+import Search from './Search.vue';
+
 export default {
-    
+    data() {
+        return {
+            title: 'Mon App',
+            username: 'ben'
+        }
+    },
+    // mounted() {
+    //     setTimeout(() => {
+    //         this.username = 'jim'
+    //     }, 2000)
+    // },
+    components: {
+        Search
+    }
 }
 </script>
---
+++ src/components/navbar/Search.vue
@@ -0,0 +1,25 @@
+<template>
+    <input type="text" v-model="localName">
+    <button @click="search">Rechercher</button>
+</template>
+
+<script lang="ts">
+export default {
+    props: ['name'],
+    data() {
+        return {
+            localName: this.name
+        }
+    },
+    methods: {
+        search() {
+            console.log(this.localName)
+        }
+    },
+    watch: {
+        name(newVal) {
+            this.localName = newVal
+        }
+    }
+}
+</script>
