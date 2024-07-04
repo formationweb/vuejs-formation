8378aef4bc3a74b9dab8987e87004e21636a00e4
RSamaium <sam@newbot.io>
2024-07-04 17:25:24 +0200

    useFecthUsers

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -1,30 +1,33 @@
 <template>
-  <h1>{{ wordPlural }}</h1>
-  <select v-model="extSelected">
-    <option value="">Tous</option>
-    <option v-for="ext in extensions" :key="ext">{{ ext }}</option>
-  </select>
-  <UserCard v-for="u in usersFiltered" :user="u" :key="u.id">
-    <template #header>
-      <h1>Titre</h1>
-    </template>
-    <template #default>
-      <p>Texte par défaut</p>
-    </template>
-    <template #footer="slotProps">
-      <p>L'utilisateur {{ slotProps.name }} est {{ slotProps.active }}</p>
-    </template>
-  </UserCard>
+  <Loading :loading>
+    <h1>{{ wordPlural }}</h1>
+    <select v-model="extSelected">
+      <option value="">Tous</option>
+      <option v-for="ext in extensions" :key="ext">{{ ext }}</option>
+    </select>
+    <UserCard v-for="u in usersFiltered" :user="u" :key="u.id">
+      <template #header>
+        <h1>Titre</h1>
+      </template>
+      <template #default>
+        <p>Texte par défaut</p>
+      </template>
+      <template #footer="slotProps">
+        <p>L'utilisateur {{ slotProps.name }} est {{ slotProps.active }}</p>
+      </template>
+    </UserCard>
+  </Loading>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios';
 import { computed, onMounted, ref } from 'vue';
 import { useExtensionFilter } from '../../composable/useExtensionFilter';
-import type { User } from '../../interfaces/User';
+import { useFetchUsers } from '../../composable/useFetchUsers';
+import Loading from '../Loading.vue';
 import UserCard from './UserCard.vue';
 
-const users = ref<User[]>([])
+const { users, loading, getAll } = useFetchUsers()
+
 const extensions: string[] = ['tv', 'biz', 'io', 'me']
 const extSelected = ref('')
 
@@ -32,8 +35,7 @@ const word = 'Utilisateur'
 const wordPlural = computed(() => word + (users.value.length > 1 ? 's' : ''))
 const usersFiltered = useExtensionFilter(users, extSelected)
 
-onMounted(async () => {
-  const res = await axios.get('https://jsonplaceholder.typicode.com/users')
-  users.value = res.data
+onMounted(() => {
+  getAll()
 })
 </script>
---
+++ src/composable/useFetchUsers.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { ref } from "vue";
+import { User } from "../interfaces/User";
+
+export function useFetchUsers() {
+  const loading = ref(false);
+  const users = ref<User[]>([])
+
+  async function getAll() {
+    try {
+        loading.value = true
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        users.value = res.data;
+    }
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+        loading.value = false
+    }
+  }
+
+  return {
+    loading,
+    users,
+    getAll
+  }
+}
