34bb12431f773f45fe273c8597fcf5e5c866bbcc
RSamaium <sam@newbot.io>
2024-07-05 12:29:51 +0200

    create user

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -1,6 +1,19 @@
 <template>
   <Loading :loading>
     <h1>{{ wordPlural }}</h1>
+    <form @submit="createUser" novalidate>
+      <label>Nom</label>
+      <input type="text" v-model="name" v-bind="nameAttrs">
+      <div>{{ errors.name }}</div>
+
+      <label>Email</label>
+      <input type="email" v-model="email" v-bind="emailAttrs">
+      <div>{{ errors.email }}</div>
+
+      <button :aria-busy="loadingCreate" :disabled="loadingCreate">
+        {{ loadingCreate ? '' : 'Créer utilisateur' }}
+      </button>
+    </form>
     <select v-model="extSelected">
       <option value="">Tous</option>
       <option v-for="ext in extensions" :key="ext">{{ ext }}</option>
@@ -21,13 +34,24 @@
 
 <script setup lang="ts">
 import axios from 'axios';
+import { useForm } from 'vee-validate';
 import { computed, onMounted, ref } from 'vue';
+import * as yup from 'yup';
 import { useExtensionFilter } from '../../composable/useExtensionFilter';
 import { useFetchUsers } from '../../composable/useFetchUsers';
 import Loading from '../Loading.vue';
 import UserCard from './UserCard.vue';
 
 const { users, loading, getAll } = useFetchUsers()
+const { handleSubmit, defineField, errors } = useForm({
+  validationSchema: {
+    email: yup.string().email().required(),
+    name: yup.string().required()
+  }
+})
+const [email, emailAttrs] = defineField('email')
+const [name, nameAttrs] = defineField('name')
+const loadingCreate = ref(false)
 
 const extensions: string[] = ['tv', 'biz', 'io', 'me']
 const extSelected = ref('')
@@ -41,6 +65,14 @@ async function deleteUser(id: number) {
   const index = users.value.findIndex(user => user.id == id)
   users.value.splice(index, 1)
 }
+
+const createUser = handleSubmit(async (values, { resetForm }) => {
+  loadingCreate.value = true
+  const { data } = await axios.post("https://jsonplaceholder.typicode.com/users", values)
+  users.value.push(data)
+  loadingCreate.value = false
+  resetForm()
+})
 
 onMounted(() => {
   getAll()
