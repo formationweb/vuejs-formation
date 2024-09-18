ffdb782a66d89505d00b3bfc3e6efbc15d0b4bd3
RSamaium <sam@newbot.io>
2024-09-18 15:36:44 +0200

    UserCard

---
+++ src/components/UserCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <article>
+        <header>{{ user.name }}</header>
+        {{ user.email }}
+    </article>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+import { User } from '../interfaces/User';
+
+export default {
+    props: {
+        user: {
+            type: Object as PropType<User>,
+            required: true
+        }
+    }
+}
+</script>
