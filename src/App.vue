c12c67c139572fde5926d3a76ac5af2ab09a0f4a
RSamaium <sam@newbot.io>
2024-09-18 11:25:48 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Navbar />
+    <h1>{{ name }}</h1>
+</template>
+
+<script lang="ts">
+import Navbar from './components/Navbar.vue'
+
+export default {
+    data() {
+        return {
+            name: 'ana'
+        }
+    },
+    components: {
+        Navbar
+    }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>My navbar</h1>
+</template>
