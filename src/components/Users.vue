a09ba323e0fefcac555d3322fc9cd5639c3ed8b3
RSamaium <sam@newbot.io>
2024-10-14 13:43:25 +0200

    usercard

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,16 @@
 <template>
     <h1>Users</h1>
+    <UserCard v-for="u in users" :user="u" />
 </template>
 
 <script lang="ts">
+import type { User } from '../interfaces/User';
+import UserCard from './UserCard.vue';
+
 export default {
-    data() {
+    data(): {
+        users: User[]
+    } {
         return {
             users: [
                 {
@@ -239,6 +245,9 @@ export default {
                 }
             ]
         }
+    },
+    components: {
+        UserCard
     }
 }
 </script>
