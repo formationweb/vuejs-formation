89391d29b722f169a185944a9b6aacab041787dc
RSamaium <sam@newbot.io>
2024-10-16 14:38:59 +0200

    UserEdit

---
+++ src/pages/UserEdit.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>User Edit</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const userId = +route.params.id
+
+console.log(userId)
+</script>
