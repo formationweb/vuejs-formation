e01c41b78decd1e54730399828523b309dd0ccac
RSamaium <sam@newbot.io>
2025-04-04 17:16:23 +0200

    Ajout du service AppService pour la gestion de la version et intégration dans l'application

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
     <router-view />
 </template>
+
+<script lang="ts" setup>
+import { inject } from 'vue';
+import { AppService } from './services/App';
+
+const service = inject<AppService>('appService')
+
+console.log(service?.getVersion())
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { router } from './router'
 import { createPinia } from 'pinia'
 import './interceptor'
 import { confirmDirective } from './directives/confirm'
+import { AppService } from './services/App'
 
 const app = createApp(App)
 
 app.directive('confirm', confirmDirective)
+app.provide('appService', new AppService())
 
 app.use(createPinia())
 app.use(router)
