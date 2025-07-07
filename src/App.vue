1d30d85902ff371acc5cc555f62ec277afeb2d5c
RSamaium <sam@newbot.io>
2025-07-07 11:53:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ message }}</h1>
+    <Navbar />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+    data() {
+        return {
+            message: 'Hello World'
+        }
+    },
+    components: {
+        Navbar
+    }
+}
+</script>
