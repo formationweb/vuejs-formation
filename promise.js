752901b17554717d7372418ab3e6e074c440bbf6
RSamaium <sam@newbot.io>
2025-07-08 15:48:12 +0200

    Ajout d'une fonction de promesse avec gestion des erreurs et exemple d'utilisation asynchrone

---
+++ promise.js
@@ -0,0 +1,44 @@
+function lancerDe(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('calcul terminé')
+           //resolve(Math.random())
+           reject(new Error('erreur'))
+        }, ms)
+    })
+}
+
+
+// pending
+// resolved / rejected
+
+// lancerDe(1000).then((nb1) => {
+//     console.log(nb1)
+//     return lancerDe(2000)
+// }).then((nb2) => {
+//     console.log(nb2)
+//     return lancerDe(2000)
+// }).then((nb3) => {
+//    console.log(nb3)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+async function foo() {
+    try {
+        const nb1 = await lancerDe(1000)
+        const nb2 = await lancerDe(1000)
+        const nb3 = await lancerDe(1000)
+        console.log(nb1, nb2, nb3)
+    }
+    catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
+foo().then(() => {
+    console.log('finish')
+}).catch((err) => {
+    console.log(err)
+})
