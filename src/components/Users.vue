927f4feb9951b9c94920fa01ef220bc3cf004746
RSamaium <sam@newbot.io>
2025-07-09 11:42:10 +0200

    Ajout d'un formulaire de création d'utilisateur avec validation via vee-validate et yup dans Users.vue

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -6,7 +6,15 @@
         <option v-for="ext in extensions" :key="ext">{{ ext }}</option>
     </select>
 
-    <Draw />
+    <form @submit.prevent="createUser">
+        <label>Nom</label>
+        <input type="text" v-model="name" v-bind="nameAttrs">
+
+        <label>Email</label>
+        <input type="text" v-model="email" v-bind="emailAttrs">
+
+        <button :aria-busy="loadingCreate" :disabled="loadingCreate">Créer utilisateur</button>
+    </form>
 
     <div v-if="!loading">
         <UserCard v-for="u in usersFiltered" :key="u.id" :user="u" @onDelete="deleteUser">
@@ -30,9 +38,34 @@ import { useExtensionFilter } from '../composables/useExtensionFilter';
 import Draw from './Draw.vue';
 import { useFetchUsers } from '../composables/useFetchUsers';
 import axios from 'axios';
+import { useForm } from 'vee-validate';
+import { object, string } from 'yup';
+import { ref } from 'vue';
 
 const { users, getAll, loading } = useFetchUsers()
 const { extSelected, extensions, usersFiltered } = useExtensionFilter(users)
+
+const { handleSubmit, defineField, resetForm } = useForm({
+    validationSchema: object({
+        name: string().required(),
+        email: string().required()
+    })
+})
+
+const loadingCreate = ref(false)
+
+const createUser = handleSubmit(async (values) => {
+    loadingCreate.value = true
+    const res = await axios.post("https://jsonplaceholder.typicode.com/users", values)
+    loadingCreate.value = false
+    const userCreated = res.data
+    // users.value.push(userCreated)
+    users.value = [...users.value, userCreated]
+    resetForm()
+})
+
+const [email, emailAttrs] = defineField('email')
+const [name, nameAttrs] = defineField('name')
 
 async function deleteUser(id: number) {
     await axios.delete("https://jsonplaceholder.typicode.com/users/" + id)
