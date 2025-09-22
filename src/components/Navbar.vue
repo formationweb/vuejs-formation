a426493339542aa1be3c41acb9f7942b60dcc927
RSamaium <sam@newbot.io>
2025-09-22 12:24:52 +0200

    Ajout du composant Search et mise à jour de Navbar pour l'inclure

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Navbar</h1>
+    <h1>{{ title }}</h1>
+    <Search />
 </template>
+
+<script>
+import Search from './Search.vue';
+
+export default {
+    data() {
+        return {
+            title: 'Mon App'
+        }
+    },
+    components: {
+        Search
+    }
+}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+    <input type="text" v-model="userName">
+    <button @click="search" v-if="userName != ''">Rechercher</button>
+    <ul>
+        <li v-for="(name, index) in firstNames" :key="index">
+          {{ index }} - {{ name }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userName: 'ana',
+            firstNames: ['ana', 'ben', 'jim']
+        }
+    },
+    methods: {
+        search() {
+            console.log(this.userName)
+        }
+    }
+}
+</script>
