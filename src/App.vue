5acf30a1750ee5ff37c7ed8716732f32a26d6056
RSamaium <sam@newbot.io>
2025-09-22 17:10:33 +0200

    Refactor App et UserCard pour utiliser la composition API de Vue. Passage à la syntaxe <script setup> et simplification de la gestion des props.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
     <Users />
 </template>
 
-<script>
+<script setup lang="ts">
 import Navbar from './components/Navbar.vue';
 import Users from './components/Users.vue';
-
-export default {
-   components: {
-        Navbar,
-        Users
-   }
-}
 </script>
