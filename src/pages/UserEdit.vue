35664d7777482a4d0c93ea7e560c5d918f8b080e
RSamaium <sam@newbot.io>
2026-04-01 15:41:08 +0200

    User Edit - part 2

--- src/pages/UserEdit.vue
+++ src/pages/UserEdit.vue
@@ -1,5 +1,15 @@
 <template>
     <h1>{{ userModifying.name }}</h1>
+
+     <form @submit.prevent="editUser">
+        <label>Email</label>
+        <input type="text" v-model="email" v-bind="emailAttrs">
+
+        <label>Nom</label>
+        <input type="text" v-model="name" v-bind="nameAttrs">
+
+        <button>Modifier utilisateur</button>
+    </form>
 </template>
 
 <script setup lang="ts">
@@ -7,13 +17,25 @@ import { useRoute } from 'vue-router';
 import { useUser } from '../composables/useUser';
 import { useUserStore } from '../store/user';
 import { storeToRefs } from 'pinia';
+import { useForm } from 'vee-validate';
 
 const user = useUser()
 const userStore = useUserStore()
-const { userModifying } = storeToRefs(userStore)
-
+const { handleSubmit, defineField, setValues } = useForm()
 const { params } = useRoute()
+
+const { userModifying } = storeToRefs(userStore)
 const userId = params.id === undefined ? null : +params.id
 
-if (userId) user.getUser(userId)
+async function run() {
+    if (userId) await user.getUser(userId)
+    setValues(userModifying.value)
+} 
+
+run()
+
+const editUser = handleSubmit(() => {})
+
+const [email, emailAttrs] = defineField('email')
+const [name, nameAttrs] = defineField('name')
 </script>
