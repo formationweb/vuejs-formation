34db29fe73b4cad05edc877e733abfdbbc0c4eb1
RSamaium <sam@webcreative5.net>
2026-06-24 14:49:20 +0200

    feat: ajouter la validation des formulaires de connexion avec vee-validate et yup

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,3 +1,43 @@
 <template>
     <h1>Login</h1>
+
+    <form @submit.prevent="login">
+        <label>Email</label>
+        <input type="text" v-model="email" v-bind="emailAttrs">
+
+        <div v-show="errors.email">
+            {{ errors.email }}
+        </div>
+
+        <label>Password</label>
+        <input type="password" v-model="password" v-bind="passwordAttrs">
+
+        <button>Se connecter</button>
+    </form>
 </template>
+
+<script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { object, string } from 'yup';
+
+const { defineField, handleSubmit, errors, setFieldValue } = useForm({
+    validationSchema: object({
+        email: string().email('Email obligatoire').required()
+            .test('email_domain', 'Nom de domaine interdit', (email: string) => {
+                return !email.endsWith('gmail.com')
+            }),
+        password: string().required().min(6)
+    })
+})
+
+const [email, emailAttrs] = defineField('email')
+const [password, passwordAttrs] = defineField('password')
+
+const login = handleSubmit((values) => {
+    console.log(values)
+})
+
+setTimeout(() => {
+    setFieldValue('email', 'zeazregrtgh@adzfegr.net')
+}, 2000)
+</script>
