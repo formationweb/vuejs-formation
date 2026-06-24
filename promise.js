c90cee9ea1ecec6f2849827b48d3b05f8f9d6876
RSamaium <sam@webcreative5.net>
2026-06-24 09:59:40 +0200

    feat: ajouter axios comme dépendance et récupérer les utilisateurs depuis une API

--- promise.js
+++ promise.js
@@ -1,16 +1,41 @@
-new Promise((resolve, reject) => {
+function lancerDe(ms) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('hello')
-    }, 1000)
-}).then((str) => {
-    console.log(str)
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('hello2')
-        }, 2000)
-    })
-}).then((str2) => {
-    console.log(str2)
-}).catch((err) => {
-    console.log(err)
-})
+      console.log("calcul terminé");
+      // resolve(Math.random())
+      reject(new Error("erreur"));
+    }, ms);
+  });
+}
+
+// lancerDe(1000).then((nb1) => {
+//     console.log(nb1)
+//     return lancerDe(2000)
+// }).then((nb2) => {
+//     console.log(nb2)
+//     return lancerDe(3000)
+// }).then((nb3) => {
+//     console.log(nb3)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+async function foo() {
+  try {
+    const nb1 = await lancerDe(1000);
+    const nb2 = await lancerDe(1000);
+    const nb3 = await lancerDe(1000);
+    console.log(nb1, nb2, nb3);
+  } catch (err) {
+    console.log('A', err);
+    throw err  
+  }
+}
+
+foo()
+  .then(() => {
+    console.log("terminé");
+  })
+  .catch((err) => {
+    console.log('B', err);
+  });
