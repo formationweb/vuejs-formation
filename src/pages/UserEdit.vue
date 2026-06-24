813f98c8c85f345117ce374449f9477a6448089e
RSamaium <sam@webcreative5.net>
2026-06-24 16:16:44 +0200

    feat: intégrer la validation des formulaires d'édition d'utilisateur avec vee-validate

--- src/pages/UserEdit.vue
+++ src/pages/UserEdit.vue
@@ -1,14 +1,38 @@
 <template>
     <h1>{{ userModifying.name }}</h1>
+
+    <form @submit.prevent="edit">
+        <label>Email</label>
+        <input type="text" v-model="email" v-bind="emailAttrs">
+
+        <label>Nom</label>
+        <input type="text" v-model="name" v-bind="nameAttrs">
+
+        <button>Modifier</button>
+    </form>
 </template>
 
 <script setup lang="ts">
 import { useUser } from '@/composables/useUser';
+import { useForm } from 'vee-validate';
 import { useRoute } from 'vue-router';
 const { get, userModifying } = useUser()
 
 const { params } = useRoute()
 const userId = params.id == undefined ? null : +params.id
 
-get(userId!)
+const { defineField, handleSubmit, setFieldValue, setValues } = useForm()
+
+const [email, emailAttrs] = defineField('email')
+const [name, nameAttrs] = defineField('name')
+
+get(userId!).then(() => {
+    // setFieldValue('email', userModifying.value.email)
+    // setFieldValue('name',  userModifying.value.name)
+    setValues(userModifying.value)
+})
+
+const edit = handleSubmit((values) => {
+    console.log(values)
+})
 </script>
