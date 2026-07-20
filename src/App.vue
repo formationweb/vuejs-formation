a9cc30d99eee863a28e4c5f663b55c310b6bdf9c
RSamaium <sam@newbot.io>
2026-07-20 11:01:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar />
+ <button v-on:click="changeTitle">Changer titre</button>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  data() {
+    return {
+      title: 'Mon App'
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.title = 'test'
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
