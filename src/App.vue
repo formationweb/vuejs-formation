e0e69890d5a457dc3d4148ab82cab3442dd54411
RSamaium <sam@newbot.io>
2026-07-20 17:25:55 +0200

    Add Draw component for canvas rendering and update App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <Navbar />
-  <Users />
+  <!-- <Navbar />
+  <Users /> -->
+  <Draw />
 </template>
 
 <script lang="ts" setup>
 import Navbar from './components/Navbar.vue';
 import Users from './components/Users.vue';
+import Draw from './Draw.vue';
 </script>
---
+++ src/Draw.vue
@@ -0,0 +1,20 @@
+<template>
+    <canvas ref="canvasRef"></canvas>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, useTemplateRef } from 'vue';
+
+const canvasEl = useTemplateRef('canvasRef')
+
+onMounted(() => {
+    const el = canvasEl.value
+    if (el) {
+        const context = el.getContext('2d')
+        if (context) {
+            context.fillStyle = 'red'
+            context.fillRect(0, 0, 100, 100)
+        }
+    }
+})
+</script>
