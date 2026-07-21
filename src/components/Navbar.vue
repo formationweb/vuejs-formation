b5d09e8e01cbff6ffc95d34a9242e91815fe04ad
RSamaium <sam@newbot.io>
2026-07-21 15:32:14 +0200

    feat: implement Main layout and update routing for home and login pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,17 @@
 <template>
     <h1>Navbar</h1>
     <Search :userName="defaultName" @onSearch="listenSearch" />
+    <!-- <RouterLink to="/login" custom v-slot="router">
+        <button @click="router.navigate">Se connecter</button>
+    </RouterLink> -->
+    <button @click="router.push({ name: 'loginId' })">Se connecter</button>
 </template>
 
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
 import Search from './Search.vue';
+
+const router = useRouter()
 
 const defaultName = 'ben'
 
