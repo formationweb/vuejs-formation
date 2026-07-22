e483a16483c7ff52abb63d6900859565a1c9d1c7
RSamaium <sam@newbot.io>
2026-07-22 15:26:10 +0200

    form user edit

--- src/pages/UserEdit.vue
+++ src/pages/UserEdit.vue
@@ -1,5 +1,15 @@
 <template>
     <h1>{{ userModifying?.name }}</h1>
+
+    <form @submit.prevent="submitEditUser">
+        <label>Email</label>
+        <input type="text" v-model="email" v-bind="emailAttrs">
+
+        <label>Nom</label>
+        <input type="password" v-model="name" v-bind="nameAttrs">
+
+        <button>Modifier utilisateur</button>
+    </form>
 </template>
 
 <script setup lang="ts">
@@ -7,14 +17,39 @@ import { useRoute } from 'vue-router';
 import { useUserStore } from '../store/user';
 import { storeToRefs } from 'pinia';
 import { useUserManage } from '../composables/useUserManage';
+import { useForm } from 'vee-validate';
+import { watchEffect } from 'vue';
 
 const route = useRoute()
 const userStore = useUserStore()
 const userManage = useUserManage()
 const { userModifying } = storeToRefs(userStore)
+const { handleSubmit, defineField, setValues } = useForm()
+
 
 const paramsId = route.params.id
 const id = paramsId ? +paramsId : undefined
 
+// async function init() {
+//     if (id) {
+//         await userManage.getUser(id)
+//         setValues(userModifying.value ?? {})
+//     }
+// }
+
+// init()
+
 if (id) userManage.getUser(id)
+
+watchEffect(() => {
+    setValues(userModifying.value ?? {})
+})
+
+const submitEditUser = handleSubmit(async (values) => {
+    
+})
+
+const [email, emailAttrs] = defineField('email')
+const [name, nameAttrs] = defineField('name')
+
 </script>
